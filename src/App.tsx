82eb2320cd6ff1d1d3d998cbb94f9a193f720bec
lukasrvs <lucas_antonio.v@hotmail.com>
2021-11-24 13:59:57 -0300

    adding files in repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { useState, useEffect, FormEvent } from 'react';
+import * as Component from './App.styles';
+import * as Photos  from './services/photos';
+import { PhotoItem } from './components/PhotoItem';
+import { Photo } from './types/Photo';
+const App = () => {
+  const [uploading, setUploading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  useEffect(()=>{
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, []);
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+    if(file && file.size > 0){
+      setUploading(true);
+        let result = await Photos.Insert(file);
+      setUploading(false);
+      if(result instanceof Error){
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  }
+  return(
+    <Component.Container>
+      <Component.Area>
+        <Component.Header>Gallery</Component.Header>
+        <Component.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image" />
+          <input className="button" type="submit" value="Send"/>
+          {uploading && "Sending..."}
+        </Component.UploadForm>
+          {loading &&
+            <Component.ScreenWarning>
+              <div className="emoji">⛔️</div>
+              <div>Loading...</div>
+            </Component.ScreenWarning>
+          }
+          {!loading && photos.length > 0 &&
+            <Component.PhotoList>
+              {photos.map((item, index)=>(
+                <PhotoItem key={index} url={item.url} name={item.name} />
+              ))}
+            </Component.PhotoList>
+          }
+          {!loading && photos.length === 0 &&
+            <Component.ScreenWarning>
+            <div className="emoji">☹️</div>
+            <div>No photos were sent.</div>
+          </Component.ScreenWarning>
+          }
+      </Component.Area>
+    </Component.Container>
   );
 }
-
 export default App;
